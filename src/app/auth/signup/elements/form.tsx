5bf8a7a9827e0d5457a5d6f0fcf260df16da1db0
Pablo Oliveira <55354322+xipanca@users.noreply.github.com>
2024-03-10 16:47:24 -0400

    feat(auth): implement signup system

--- src/app/auth/signup/elements/form.tsx
+++ src/app/auth/signup/elements/form.tsx
@@ -5,31 +5,47 @@ import { Input } from "@/components/ui/input";
 import { useFormStatus, useFormState } from "react-dom";
 import { createUser } from "./actions";
 import { Loader } from "lucide-react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function SignupForm() {
-  const initialState = {
+  const router = useRouter();
+  const initState = {
     message: "",
+    success: false,
   };
-  const [state, formAction] = useFormState(createUser, initialState);
-  const { pending } = useFormStatus();
+  const [state, formAction] = useFormState(createUser, initState);
+  useEffect(() => {
+    if (state.success) {
+      router.push("/auth/signin");
+    }
+  }, [state.message, router]);
   return (
     <form className="flex flex-col gap-4" action={formAction}>
       <h1 className="text-2xl font-bold text-center">Sign Up</h1>
-      <Input name="name" type="text" placeholder="Name" />
+      <Input name="username" type="text" placeholder="Name" />
       <Input name="email" type="text" placeholder="Email" />
       <Input name="password" type="password" placeholder="Password" />
       <Input
-        name="confirmpassword"
+        name="confirmPassword"
         type="password"
         placeholder="Confirm Password"
       />
-      <Button
-        type="submit"
-        aria-disabled={pending}
-        className={pending ? "bg-slate-950" : ""}
-      >
-        {pending ? <Loader /> : "Sign Up"}
-      </Button>
+      <LoginButton />
       <p className="font-light text-center text-red-400"> *{state.message}</p>
     </form>
+  );
+}
+
+function LoginButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      aria-disabled={pending}
+      className={pending ? "bg-slate-950" : ""}
+    >
+      {pending ? <Loader /> : "Sign Up"}
+    </Button>
   );
 }
